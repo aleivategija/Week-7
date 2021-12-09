5d53d62a35e48a829770b882da333eb08f6644c1
Artur <aleivate@tktk.ee>
2021-12-09 20:18:51 +0200

    Week 7

---
+++ UserNameDataTask/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace UserNameDataTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda eesnimi:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Sisesta numbrina, millist funktsiooni soovid (1, 2 voi 3)");
+            char userChoice = Convert.ToChar(Console.ReadLine());
+
+            switch(userChoice)
+            {
+                case '1':
+                    FirstNameReverse(userName);
+                    break;
+                case '2':
+                    FirstNameLetter(userName);
+                    break;
+                case '3':
+                    FirstNameLength(userName);
+                    break;
+            }
+           
+        }
+
+        public static void FirstNameReverse(string userName)
+        {
+            for (int i = userName.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userName[i]);
+            }
+                
+        }
+
+        public static void FirstNameLetter(string userName)
+        {
+            Console.WriteLine($"Sinu eesnime esimene taht on {userName[0]}");
+        }
+
+        public static void FirstNameLength(string userName)
+        {
+            Console.WriteLine($"Sinu nimi on {userName.Length} sumbolit pikk.");
+        }
+    }
+}
---
+++ userNumberData/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace userNumberData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sisesta oma eesnimi:");
+            string userName = Console.ReadLine();
+
+            GetUserNameData(userName);
+
+
+        }
+
+        public static void GetUserNameData(string userInput)
+        {
+            Console.WriteLine($"Sinu nimi on {userInput.Length} sumbolit pikk.");
+            Console.WriteLine($"Sinu eesnime esimene taht on {userInput[0]}");
+
+            for(int i = userInput.Length - 1; i >= 0; i--)
+            {
+                Console.Write(userInput[i]);
+            }
+
+        }
+
+    }
+
+}
